e24e4fe955f2e2a2e281f5cab37d4df00e565682
bhenbe <contact@bhen.be>
2018-10-13 07:10:00 +0200

    Adding API Context Data to post list

---
+++ components/26-post-list/post-list.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const request = require('request-promise'); // require the request-promise module
+
+// make the request to the API, returns a Promise
+var options = {
+    uri: 'https://jsonplaceholder.typicode.com/photos?_start=0&_limit=200',
+    json: true // Automatically parses the JSON string in the response
+};
+
+// do some post-processing on the response to wrangle it into the correct format
+var testData = [];
+request(options).then(function (testApiData) {
+    for (let test of testApiData) {
+        testData.push({
+            title: test.title,
+            url: test.url
+        });
+    }
+    return testData;
+}).catch(function (err) {
+    console.log(err);
+});
+
+module.exports = {
+    title: "Post card",
+    status: "ready",
+    default: "default",
+	context: {
+		posts: testData // use the response as context data for our template.
+	}
+};
